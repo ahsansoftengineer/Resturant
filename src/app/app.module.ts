c892478caa7791a5f1fa2a5c092bc6e6498f7793
Muhammad Ahsan <ahsansoftengineer>
2021-01-24 19:35:06 +0500

    Part 42 Getting the Index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Required for Two way databinding
   ],
   providers: [],
   bootstrap: [AppComponent]
