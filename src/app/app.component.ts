065231620df32b22be19281a5203eae2b7dda9f9
Muhammad Ahsan <ahsansoftengineer>
2021-01-25 08:46:59 +0500

    Resturant Project Files Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,44 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Resturant';
-  allowNewServer = true;
-  porp : string = 'Pre defined text';
-  a = false;
-  bcolor = '';
-  red = 'red';
-  green = 'green'
-  printedText: string;
-  serverOnline = false;
-  fruits:string[] = ['apple', 'banana', 'orange', 'mango'];
-  dpara = false; // Assignment ngIf Para
-
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer = false;
-    }, 3000);
-    this.log.pop();
-}
-  changeText = 'Old text';
-  conditionallyLoads = false;
-  myfunc() {
-    this.changeText = 'New text';
-  }
-  myDollar(event: Event) {
-    this.printedText = <string>(<HTMLInputElement>event.target).value;
-  }
-  // Part 38 Styling Elements
-  serverfunc(){
-    this.serverOnline = Math.random() > 0.5;
-    this.bcolor = Math.random() > 0.5 ? 'green' : 'red';
-   }
-  // Part 40 ngFor
-  addFruit(){
-    this.fruits.push('çhiku')
-  }
-  i:number = 0;
-  log = [{sNo:0 ,date: Date.now()}]
-  logInfo(){
-    this.dpara = !this.dpara;
-    this.log.push({ sNo: ++this.i, date: Date.now()})
+
   }
 }
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-recipes',
+  templateUrl: '../recipes/recipes.component.html',
+  styleUrls: ['../recipes/recipes.component.css']
+})
+export class RecipesComponent{
+  constructor(){
+
+  }
+}
