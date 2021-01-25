fa697c8726953b669b6d275da31ed65b177198a2
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2021-01-25 18:51:01 +0500

    Section 4 Course Project Completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model"
 
 @Component({
   selector:'app-recipe-list',
@@ -7,6 +8,17 @@ import { Component } from "@angular/core";
 })
 export class RecipeListComponent{
   constructor(){
-
   }
+  recipies : Recipe[] = [
+    new Recipe(
+      'Chana Chaat','Cholay','assets/Images/1.jpeg'
+    ),
+    new Recipe(
+      'Chana Chaat','Cholay','assets/Images/2.jpeg'
+    ),
+    new Recipe(
+      'Chana Chaat','Cholay','assets/Images/3.jpeg'
+    ),
+  ]
+
 }
