07edd3c6afd46b084c52210726717bba84aa0973
King <ahsansoftengineer>
2021-01-28 13:07:38 +0500

    Pragim Tech

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,27 @@
-import {
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  ContentChild,
-  DoCheck,
-  ElementRef,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent
-  implements
-    AfterViewChecked,
-    AfterContentInit,
-    AfterViewInit,
-    AfterViewChecked,
-    DoCheck,
-    OnChanges,
-    OnInit,
-    OnDestroy {
+export class AppComponent implements OnInit {
+  employee: Employee;
+  employees: Employee[];
   constructor() {
     console.warn('Constructor Called!');
+    this.employee = new Employee(105, 'a', 'b', 'c')
+    this.employees.push
+    this.employee.EmployeeList();
   }
-  @ViewChild('viewchildparaEG') viewchildparadec: ElementRef;
-  @ContentChild('contentchildparaEG') contentchildparadec: ElementRef;
-  ngOnInit() {
-    console.log('OnInit Called!');
-  }
-  ngDoCheck() {
-    console.warn('DoCheck Called!');
-  }
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('OnChanges Called!' + changes);
-  }
-  ngAfterContentInit() {
-    console.log('AfterContentInit Called!');
-    // console.log(this.contentchildparadec.nativeElement.textContent);
-  }
-  ngAfterContentChecked() {
-    console.log('AfterContentChecked Called!');
-  }
-  ngAfterViewInit() {
-    console.log('AfterViewInit Called!');
-    console.log(this.viewchildparadec.nativeElement.textContent);
-  }
-  ngAfterViewChecked() {
-    console.log('AfterViewChecked Called!');
-  }
+  ngOnInit(){
 
-  ngOnDestroy() {
-    console.warn('OnDestroy Called!');
   }
-
-  DestroyedFirst() {
-    this.serverElements.splice(0, 1);
-  }
-
-  title = 'Resturant';
-  serverElements = [
-    { type: 'server', name: 'Test Server', content: 'Just a Test' },
-  ];
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-  onBlueprintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
-    });
+  addEmployee(){
+    this.employee.EmployeeList().push(
+      new Employee(106, 'Rabia','female','02-02-2002')
+    )
+    console.log(this.employee.EmployeeList())
   }
 }
