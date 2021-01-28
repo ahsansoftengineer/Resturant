fb6775244a54a0576f851b66429bd6bdc1fe01cc
King <ahsansoftengineer>
2021-01-28 09:28:31 +0500

    Events Cycle Have been Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+  implements
+    AfterViewChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked,
+    DoCheck,
+    OnChanges,
+    OnInit,
+    OnDestroy {
+  constructor() {
+    console.warn('Constructor Called!');
+  }
+  @ViewChild('viewchildparaEG') viewchildparadec: ElementRef;
+  @ContentChild('contentchildparaEG') contentchildparadec: ElementRef;
+  ngOnInit() {
+    console.log('OnInit Called!');
+  }
+  ngDoCheck() {
+    console.warn('DoCheck Called!');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('OnChanges Called!' + changes);
+  }
+  ngAfterContentInit() {
+    console.log('AfterContentInit Called!');
+    // console.log(this.contentchildparadec.nativeElement.textContent);
+  }
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked Called!');
+  }
+  ngAfterViewInit() {
+    console.log('AfterViewInit Called!');
+    console.log(this.viewchildparadec.nativeElement.textContent);
+  }
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked Called!');
+  }
+
+  ngOnDestroy() {
+    console.warn('OnDestroy Called!');
+  }
+
+  DestroyedFirst() {
+    this.serverElements.splice(0, 1);
+  }
+
   title = 'Resturant';
   serverElements = [
     { type: 'server', name: 'Test Server', content: 'Just a Test' },
@@ -17,7 +76,10 @@ export class AppComponent {
       content: serverData.serverContent,
     });
   }
-  onBlueprintAdded(blueprintData: { serverName: string; serverContent: string; }) {
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
