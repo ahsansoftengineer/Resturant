c6df36859a94f1da69c6aed8f44f9f5599f0a3ab
M Ahsan <ahsan.moeen@indusvalley.io>
2021-02-28 23:59:20 +0500

    Custom PreLoading Setting App Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import {
+  Routes,
+  RouterModule,
+  // PreloadAllModules,
+  // NoPreloading,
+} from '@angular/router';
+import { CustomPreloadingServiceService } from './custom-preloading-service.service';
 import { HomeComponent } from './home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
+const setPreLoad: boolean = false;
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   // {path: 'employees', loadChildren: './employees/employees.module#EmployeesModule'}
   {
     path: 'employees',
+    data: { preload: setPreLoad },
     loadChildren: () =>
       import('./employees/employees.module').then((m) => m.EmployeesModule),
   },
@@ -18,9 +25,13 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
+      preloadingStrategy: CustomPreloadingServiceService, // Only the Specified Module Pre Load
+      // preloadingStrategy: NoPreloading, // Never PreLoad
+      // preloadingStrategy: PreloadAllModules, // Always Preload All Module
     }),
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  // setPreLoad = true; // Cannot Conditionally set value here
+}
