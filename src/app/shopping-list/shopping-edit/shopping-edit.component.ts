6677941eb06a859f22b1639ab27b6914d7382313
M Ahsan <ahsan.moeen@indusvalley.io>
2021-03-14 04:14:19 +0500

    Part 90 Adding Item Using Template Ref Var

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-    import { AfterContentInit, Component, ContentChild, ElementRef, OnInit } from '@angular/core';
+    import { AfterContentInit, Component, ContentChild, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
     @Component({
       selector: 'app-shopping-edit',
@@ -6,13 +7,24 @@
       styleUrls: ['./shopping-edit.component.css']
     })
     export class ShoppingEditComponent implements OnInit, AfterContentInit {
+      @ContentChild('contentChild') accessChildContent: ElementRef;
+      constructor(){}
+      @ViewChild('name') itemName: ElementRef;
+      @ViewChild('amount') amount: ElementRef;
+      @Output()ingredientEmitter = new EventEmitter<Ingredient>();
+
       ngOnInit(): void {
         // console.log('ContentChild = ' + this.accessChildContent.nativeElement.textContent);
       }
-      @ContentChild('contentChild') accessChildContent: ElementRef;
       ngAfterContentInit(): void {
         //Called after ngOnInit when the component's or directive's content has been initialized.
         //Add 'implements AfterContentInit' to the class.
         console.log('ContentChild = ' + this.accessChildContent.nativeElement.textContent);
+      }
+      AddItems(){
+        const itemname = this.itemName.nativeElement.value
+        const amount = this.amount.nativeElement.value
+        console.log(itemname + '  ' + amount)
+        this.ingredientEmitter.emit(new Ingredient(itemname, amount))
       }
     }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,42 +1,46 @@
-    import {
-      Component,
-      ContentChild,
-      ElementRef,
-      OnInit,
-      ViewChild,
-      // ViewEncapsulation
-    } from '@angular/core';
-    import { Ingredient } from '../shared/ingredient.model';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+  // ViewEncapsulation
+} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
-    @Component({
-      selector: 'app-shopping-list',
-      templateUrl: './shopping-list.component.html',
-      styleUrls: ['./shopping-list.component.css'],
-      // encapsulation: ViewEncapsulation.Emulated
-    })
-    export class ShoppingListComponent implements OnInit {
-      ingredients: Ingredient[] = [
-        new Ingredient('Apple', 10),
-        new Ingredient('Carrot', 5)
-      ];
-      name = "Ahsan"
-      @ViewChild('heading') headingDiv: ElementRef
-      @ContentChild('contentChild') accessChildContent: ElementRef;
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+  // encapsulation: ViewEncapsulation.Emulated
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 10),
+    new Ingredient('Carrot', 5),
+  ];
+  name = 'Ahsan';
+  @ViewChild('heading') headingDiv: ElementRef;
+  @ContentChild('contentChild') accessChildContent: ElementRef;
 
-      constructor() { }
-      ngOnInit(): void {
-        //  console.log('Div Heading = ' + this.headingDiv.nativeElement.textContent);
-        // console.log('@ContentChild = ' + this.accessChildContent.nativeElement.textContent);
-      }
-      ngAfterContentInit(): void {
-        //Called after ngOnInit when the component's or directive's content has been initialized.
-        //Add 'implements AfterContentInit' to the class.
-        // console.log('ContentChild = ' + this.accessChildContent.nativeElement.textContent);
-      }
-      ngAfterViewInit(): void {
-        // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-        // Add 'implements AfterViewInit' to the class.
-        console.log('Div Heading = ' + this.headingDiv.nativeElement.textContent);
+  constructor() {}
 
-      }
-    }
+  pushIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+  ngOnInit(): void {
+    //  console.log('Div Heading = ' + this.headingDiv.nativeElement.textContent);
+    // console.log('@ContentChild = ' + this.accessChildContent.nativeElement.textContent);
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    // console.log('ContentChild = ' + this.accessChildContent.nativeElement.textContent);
+  }
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    console.log('Div Heading = ' + this.headingDiv.nativeElement.textContent);
+  }
+}
